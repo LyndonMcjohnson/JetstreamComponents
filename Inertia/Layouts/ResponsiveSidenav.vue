5eded27c3f1056a90978abdd38de3ac8335db0ee
Lyndon <mcjohnsonlyndon@gmail.com>
2021-08-05 11:52:44 +0000

    Update ResponsiveSidenav.vue

--- Inertia/Layouts/ResponsiveSidenav.vue
+++ Inertia/Layouts/ResponsiveSidenav.vue
@@ -23,9 +23,9 @@
                         <svg class="w-6 h-6 inline-block -mt-1 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M16 8v8m-4-5v5m-4-2v2m-2 4h12a2 2 0 002-2V6a2 2 0 00-2-2H6a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
                         Dashboard
                     </inertia-link>
-                    <inertia-link :href="'#'" class="block py-2.5 px-4 hover:bg-blue-100 text-gray-800 transition duration:100 rounded-lg" :class="route().current('') ? 'border-r-8 border-blue-700 bg-blue-100 text-blue-700' : ''">
+                    <inertia-link :href="'#'" class="block py-2.5 px-4 hover:bg-blue-100 text-gray-800 transition duration:100 rounded-lg" :class="route().current('products') ? 'border-r-8 border-blue-700 bg-blue-100 text-blue-700' : ''">
                         <svg class="w-6 h-6 inline-block -mt-1 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                        Events
+                        Products
                     </inertia-link>
                     <inertia-link href="#" class="block py-2.5 px-4 hover:bg-blue-100 text-gray-800 transition duration:100 rounded-lg">
                         <svg class="w-6 h-6 inline-block -mt-1 mr-2" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v2m0 4v2m0 4v2M5 5a2 2 0 00-2 2v3a2 2 0 110 4v3a2 2 0 002 2h14a2 2 0 002-2v-3a2 2 0 110-4V7a2 2 0 00-2-2H5z"></path></svg>
@@ -54,12 +54,12 @@
                                 <!-- Navigation Links -->
                                 <div class="hidden space-x-8 sm:-my-px sm:flex items-center">
                                     <!-- jetstream native navlinks -->
-                                    <!-- <jet-nav-link :href="route('events')" :active="route().current('events')">
-                                        Link
+                                    <!-- <jet-nav-link :href="'#'" :active="route().current('products')">
+                                        Products
                                     </jet-nav-link> -->
-                                    <inertia-link :href="route('events.create')" class="text-xs text-blue-700 border border-blue-700 rounded-lg px-4 py-2 hover:bg-blue-700 hover:text-white font-black transition duration-100">
+                                    <inertia-link :href="'#'" class="text-xs text-blue-700 border border-blue-700 rounded-lg px-4 py-2 hover:bg-blue-700 hover:text-white font-black transition duration-100">
                                         <svg class="w-5 h-5 inline-block -mt-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"></path></svg>
-                                        CREATE EVENT
+                                        NEW PRODUCT
                                     </inertia-link>
                                 </div>
                             </div>
